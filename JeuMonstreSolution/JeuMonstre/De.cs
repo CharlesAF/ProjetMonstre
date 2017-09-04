567ae6fb391823310e3b1bbf1ccd1146aad90577
KenJoelTL <joellutumba@hotmail.com>
2017-09-03 22:40:11 -0400

    ajout de la classe statique De

---
+++ JeuMonstreSolution/JeuMonstre/De.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JeuMonstre
+{
+    //classe de service
+    static class De
+    {
+        private static Random valeurAleatoire = new Random();
+        
+
+        //Retourne une valeur aléatoire à chaque fois que la méthode statique est appelée
+        public static int LanceLeDe()
+        {
+            return valeurAleatoire.Next(1,7);
+        }
+
+    }
+}
