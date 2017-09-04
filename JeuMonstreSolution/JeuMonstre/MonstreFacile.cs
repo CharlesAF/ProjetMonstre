e1d203ebe29dbad092cf252ecf04186b96f6641a
KenJoelTL <joellutumba@hotmail.com>
2017-09-03 23:10:40 -0400

    ajout de la classe MonstreFacile

---
+++ JeuMonstreSolution/JeuMonstre/MonstreFacile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JeuMonstre
+{
+    class MonstreFacile
+    {
+        public bool EstVivant { get; private set; }
+        private const int FORCE = 10;   //Dégats qu'un monstreFacile inflige lorsqu'il attaque
+
+        public MonstreFacile()
+        {
+            this.EstVivant = true;
+        }
+
+        public void Attaque(Joueur joueur)
+        {
+            int lanceJoueur = joueur.LanceLeDe();
+            int lanceMonstre = this.lanceLeDe();
+            if (lanceMonstre > lanceJoueur)
+            {
+                joueur.SubitDegats(FORCE);
+            }
+
+        }
+
+        public void subitDegats()
+        {
+            this.EstVivant = false;
+        }
+
+        public int lanceLeDe()
+        {
+            return De.LanceLeDe();
+        }
+    }
+}
